9fd073c1953c0f7cc17bae8c7f09af07742cea24
aphophal <aphophal@andrew.cmu.edu>
2011-02-14 19:38:15 -0500

    starting the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChoreTracker::Application.config.secret_token = 'bd4956b79f8aaacdaad4316d836dab5f96f005cb16acb5d05658a619db62ffe1f7d2c7ff276a4f76107de40fe78f6efa8f369e5f893e239ce51152312def2b30'
